49f966278ec30d4f84315c5de38cb04d60318c73
Fabian Reisegger <fabian.reisegger@students.fhv.at>
2018-04-30 13:48:11 +0200

    WiP

--- tests/client_tests/php/postgres_pdo/test_postgres_pdo.php
+++ tests/client_tests/php/postgres_pdo/test_postgres_pdo.php
@@ -8,39 +8,52 @@
 //}
 //$host = $argv[1];
 //$port = $argv[2];
-
-$host = "localhost";
-$port = "5432";
-
+//
 //
 //if(empty($host) || empty($port)) {
 //    echo "Provide both host and port of CrateDB";
 //    exit(1);
 //}
 
+$host = 'localhost';
+$port = 5432;
+
+//BASIC TEST
 $pdo = new PDO('pgsql:dbname=doc;user=crate;host='.$host.';port='.$port);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//create
-$stmt = $pdo->query("create table pdo_test (x int, y string)");
-
-//insert
-$stmt = $pdo->prepare("insert into pdo_test (x, y) values (?, ?)");
-$stmt->execute([1, "Postgres"]);
-
-//select
-$select = "select x, y from pdo_test";
-foreach ($pdo->query($select) as $row) {
-    print $row['x']."\n";
-    print $row['y']."\n";
+$stmt = $pdo->query("select name from sys.cluster");
+while ($row = $stmt->fetch()) {
+    echo "Cluster name: ".$row['name']."\n";
 }
 
-//delete
-$stmt = $pdo->query("delete from pdo_test where x = 1");
+$create = $pdo->prepare("create table pdo_test (x int, y string)");
+$create->execute();
 
+$insert = $pdo->prepare("insert into pdo_test (x, y) values (?, ?)");
+$insert->execute([1, "Postgres"]);
 
+$pdo->prepare("refresh table pdo_test")->execute();
+$stmt = NULL;
 
-//drop
+$select = $pdo->query("select x, y from pdo_test");
+while ($row = $select->fetch()) {
+    echo "x row: ".$row['x']."\n";
+    echo "y row: ".$row['y']."\n";
+}
+
+$delete = $pdo->prepare("delete from pdo_test where x = 1");
+$delete->execute();
+
 $stmt = $pdo->query("drop table pdo_test");
 
+//SSL TEST
+//phpinfo();
+$pdo = NULL;
+try {
+    $pdo = new PDO('pgsql:dbname=doc;user=crate;host=' . $host . ';port=' . $port);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+    print $e->getMessage();
+}
 ?>
